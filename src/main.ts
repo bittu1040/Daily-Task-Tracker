2ebe18b9b2bab11ef9d441e0f43a0f51584b7794
Bittu <bittu1040@gmail.com>
2025-07-20 23:49:17 +0530

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
+
+inject(); // Initialize Vercel Analytics
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
